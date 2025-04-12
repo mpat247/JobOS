aea42287aec9618f4e3081e89557d1ba9d12d0d2
Manav Patel <64374123+mpat247@users.noreply.github.com>
2025-04-12 13:41:04 -0400

    initial commit

---
+++ app/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello World from JobOS</h1>
+    </main>
+  );
+}
