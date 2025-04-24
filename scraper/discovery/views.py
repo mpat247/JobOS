70a8120094a10394e2d3e793ebf06e54c6eb6436
Manav Patel <64374123+mpat247@users.noreply.github.com>
2025-04-24 12:43:29 -0400

    updated docker files, deploying to aws app runner

--- scraper/discovery/views.py
+++ scraper/discovery/views.py
@@ -1,21 +1,23 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
-from .helpers.career_site_search import get_career_links, select_correct_site
+from .helpers.career_site_search import getCareerLinks, selectCorrectSite
 
 @require_GET
-def discover_view(request):
+def discoverView(request):
     company = request.GET.get("q", "").strip()
+    print(f"Company: {company}")
     if not company:
         return JsonResponse({"error": "Missing query ?q=..."}, status=400)
-
+    
     try:
-        results = get_career_links(company, limit=5)
-        print(f"Results for {company}: {results}") 
-        final_site = select_correct_site(results)
+        results = getCareerLinks(company, limit=5)
+        print(f"Results for {company}: {results}")
+        final_site = selectCorrectSite(results)
         print(f"Final site for {company}: {final_site}")
         return JsonResponse({
             "company": company,
-            "results": results
+            "results": results,
+            "final": final_site
         })
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
